4604c02431e8c7de85ce42ddf5537ec1c00bd814
JUMPSTONIK <xtineen@gmail.com>
2018-03-05 12:52:52 -0600

    subiendo proyecto

---
+++ HDT6/src/hdt6/HDT6.java
@@ -0,0 +1,22 @@
+/*
+ * Universidad del Valle de Guatemala
+ * Josue David Lopez 17081
+ * Rodrigo Morales 17027
+ * 
+ */
+package hdt6;
+
+/**
+ *
+ * @author JUMPSTONIK
+ */
+public class HDT6 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
