b90e65c74b1f1798629c7b94de6e3594951de25d
Rjmorcast <32773693+Rjmorcast@users.noreply.github.com>
2018-03-08 20:18:36 -0600

    Add files via upload
    Ya esta hecho el factory y la clase de tipo carta

---
+++ HDT6/src/hdt6/Carta.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt6;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class Carta {
+    public String Nombre;
+    public String Tipo;
+    
+    public Carta(String Nombre, String Tipo){
+        this.Nombre = Nombre;
+        this.Tipo = Tipo;
+    }
+}
---
+++ HDT6/src/hdt6/CartasFactory.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hdt6;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.TreeMap;
+
+/**
+ *
+ * @author Rodrigo
+ */
+public class CartasFactory {
+    public static Map setTypeCartas(int tipo) {
+        Map<String, String> Mapa;
+        //HashMap
+        if (tipo == 1){
+            Mapa = new HashMap<String, String>();
+        } else if (tipo == 2){
+            //TreeMap
+            Mapa = new TreeMap<String, String>();
+        } else {
+            //LinkedHashMap
+            Mapa = new LinkedHashMap<String, String>();
+        }
+        return Mapa;
+    }
+}
